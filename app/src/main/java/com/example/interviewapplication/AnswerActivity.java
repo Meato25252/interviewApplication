a60158937b1fad6e8fab4f020234b55f690ff888
meato <ta.o-0804@outlook.jp>
2025-08-07 15:59:27 +0900

    音声入力機能追加

--- app/src/main/java/com/example/interviewapplication/AnswerActivity.java
+++ app/src/main/java/com/example/interviewapplication/AnswerActivity.java
@@ -1,7 +1,13 @@
 package com.example.interviewapplication;
 
+import static android.Manifest.permission.RECORD_AUDIO;
+
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.EditText;
@@ -10,11 +16,14 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.room.Room;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -26,13 +35,103 @@ public class AnswerActivity extends AppCompatActivity{
     private AppDatabase db;
     private long nowId;
     private TextToSpeech textToSpeech;
+    private SpeechRecognizer speechRecognizer;
+    private Boolean flag=true;
+    private Intent intentR;
+
+    ArrayList<String> matches;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
 
+        if (ContextCompat.checkSelfPermission(this, RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, RECORD_AUDIO)) {
+                // 拒否した場合
+            } else {
+                // 許可した場合
+                int MY_PERMISSIONS_RECORD_AUDIO = 1;
+                ActivityCompat.requestPermissions(this, new String[]{RECORD_AUDIO}, MY_PERMISSIONS_RECORD_AUDIO);
+            }
+        }
 
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(AnswerActivity.this);
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+                System.out.println("SpeechRecognizer エラー: " + error);
+                switch (error) {
+                    case SpeechRecognizer.ERROR_AUDIO:
+                        System.out.println("ERROR_AUDIO");
+                        break;
+                    case SpeechRecognizer.ERROR_CLIENT:
+                        System.out.println("ERROR_CLIENT");
+                        break;
+                    case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                        System.out.println("ERROR_INSUFFICIENT_PERMISSIONS");
+                        break;
+                    case SpeechRecognizer.ERROR_NETWORK:
+                        System.out.println("ERROR_NETWORK");
+                        break;
+                    case SpeechRecognizer.ERROR_NO_MATCH:
+                        System.out.println("ERROR_NO_MATCH");
+                        break;
+                    case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                        System.out.println("ERROR_RECOGNIZER_BUSY");
+                        break;
+                    default:
+                        System.out.println("その他のエラー: " + error);
+                }
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if (matches != null && !matches.isEmpty()) {
+                    System.out.println("Speech" + "認識結果: " + matches.get(0));
+                }else{
+                    System.out.println("null");
+                }
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+        });
+
+        question = getIntent().getParcelableExtra("id");
 
         textToSpeech= new TextToSpeech(AnswerActivity.this, new TextToSpeech.OnInitListener() {
             @Override
@@ -47,7 +146,6 @@ public class AnswerActivity extends AppCompatActivity{
             }
         });
 
-        question = getIntent().getParcelableExtra("id");
         textView = (TextView) findViewById(R.id.text);
 
         System.out.println("■："+question);
@@ -58,6 +156,11 @@ public class AnswerActivity extends AppCompatActivity{
         LocalDataDao localDataDao = db.localDataDao();
         Data data = new Data();
 //        LocalDataStore localDataStore = new LocalDataStore();
+
+        intentR = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intentR.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        intentR.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intentR.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
 
 
         findViewById(R.id.button1).setOnClickListener(
@@ -74,13 +177,17 @@ public class AnswerActivity extends AppCompatActivity{
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        editText = findViewById(R.id.edittext);
-                        String text = editText.getText().toString();
+//                        editText = findViewById(R.id.edittext);
+//                        String text = editText.getText().toString();
 //                        localDataStore.add(text);
 
                         new Thread(()-> {
                             //room
-                            data.text1 = text;
+                            if(matches == null || matches.isEmpty()){
+                                data.text1="入力がされませんでした。";
+                            }else{
+                                data.text1 = matches.get(0);
+                            }
                             data.tag = question.getText();
                             nowId=localDataDao.insertAll(data);
                             System.out.println(localDataDao.getAll());
@@ -93,6 +200,20 @@ public class AnswerActivity extends AppCompatActivity{
                                 startActivity(intent2);
                             });
                         }).start();
+                    }
+                }
+        );
+
+        findViewById(R.id.button3).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        System.out.println("■"+flag);
+                        System.out.println("aaaa");
+                        speechRecognizer.startListening(intentR);
+//                        }else{
+//                            speechRecognizer.stopListening();
+//                            flag=true;
                     }
                 }
         );
@@ -111,6 +232,8 @@ public class AnswerActivity extends AppCompatActivity{
         super.onDestroy();
         textToSpeech.stop();
         textToSpeech.shutdown();
+        speechRecognizer.cancel();
+        speechRecognizer.destroy();
     }
 
 }
